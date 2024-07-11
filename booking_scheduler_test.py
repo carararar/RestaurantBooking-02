89892e5e0eb4b9db45d92a5b00dcf8802e807844
car <znargon@gmail.com>
2024-07-11 11:08:36 +0900

    [feature] step6

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 
 from booking_scheduler import BookingScheduler
 from schedule import Customer, Schedule
+from testable_sms_sender import TestableSmsSender
 
 NOT_ON_THE_HOUR = datetime.strptime('2021/03/26 09:05', '%Y/%m/%d %H:%M')
 ON_THE_HOUR = datetime.strptime('2021/03/26 09:00', '%Y/%m/%d %H:%M')
@@ -35,7 +36,8 @@ class BookingSchedulerTest(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
             self.booking_scheduler.add_schedule(new_schedule)
-        self.assertEqual("Number of people is over restaurant capacity per hour", str(context.exception))
+        self.assertEqual("Number of people is over restaurant capacity per hour",
+                         str(context.exception))
 
     def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(self):
         schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER)
@@ -48,7 +50,12 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertTrue(self.booking_scheduler.has_schedule(schedule))
 
     def test_예약완료시_SMS는_무조건_발송(self):
-        pass
+        testable_sms_sender = TestableSmsSender()
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        self.booking_scheduler.set_sms_sender(testable_sms_sender)
+
+        self.booking_scheduler.add_schedule(schedule)
+        self.assertTrue(testable_sms_sender.is_send_method_is_called())
 
     def test_이메일이_없는_경우에는_이메일_미발송(self):
         pass
